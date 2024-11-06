87aa7512f550e3f360a9ec70799105e650c20b59
lisa-von-mond <lisalue@posteo.de>
2024-11-06 17:35:08 +0100

    create explanation tooltip

--- src/components/widgets/Weather.vue
+++ src/components/widgets/Weather.vue
@@ -57,7 +57,7 @@ axios.get('http://api.weatherapi.com/v1/current.json?key=efb8776062704d21bcc1249
         console.log(weatherData)
         // push only air quality data into data array for widget component
         // state.airQualityData = responseData.air_quality
-        
+
       }
     })
     .catch(error => {console.log(error)})
@@ -66,26 +66,34 @@ axios.get('http://api.weatherapi.com/v1/current.json?key=efb8776062704d21bcc1249
 </script>
 
 <template>
+
   <div id="weather" class="weather" v-if="state.weatherData !== null">
     <h3>Wetter Berlin</h3>
       <div class="weather__content">
+
       <div class="weather__section weather__section-warmth">
         <p>{{ state.weatherData.temp_c }}°</p>
         <hr>
         <p> {{ state.weatherData.feelslike_c }}°</p>
       </div>
+
       <div class="weather__section weather__section-condition">
-        <!---p>{{ state.weatherData.condition.text }}</p--->
-        <img :src="state.weatherData.condition.icon"/>
+        <span class="hover-area">
+          <img :src="state.weatherData.condition.icon"/>
+          <div class="tooltip">{{ state.weatherData.condition.text }}</div>
+        </span>
       </div>
+
       <div class="weather__section weather__section-wind">
         <p>{{ state.weatherData.wind_kph }} <span class="smaller-text">km/h</span></p>
         <hr>
         <p :style="{'transform': `rotate(${windDirection.angle}deg)`}">↑</p>
-        <p>{{ windDirection.direction }}</p>
+        <!---p>{{ windDirection.direction }}</p--->
       </div>
-    </div>
+
+      </div>
   </div>
+
 </template>
 
 <style lang='scss' scoped>
@@ -125,5 +133,31 @@ axios.get('http://api.weatherapi.com/v1/current.json?key=efb8776062704d21bcc1249
   }
 }
 
+/* hover areas and tooltips - may be moved to global leven */
+
+.hover-area {
+  position: relative;
+  display: block;
+
+.tooltip {
+  position: absolute;
+  right: 2rem;
+  top: -1rem;
+  transform: translate(100%, 100%);
+  padding: 0.2rem;
+  background: $color-secondary--dark;
+  color: $color-accent;
+  font-size: 0.8rem;
+  text-transform: lowercase;
+  white-space: nowrap;
+  letter-spacing: 0.2rem;
+  transition: opacity 0.3s;
+  opacity: 0;
+}
+
+&:hover .tooltip{
+    opacity: 1;
+  }
+}
 
 </style>
